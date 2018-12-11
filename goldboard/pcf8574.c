48b6aebbb8fb93b0018e7861def8e5ba174190a3
Alexander Ulbrich <alexander.ulbrich@uni-ulm.de>
2018-12-11 14:38:26 +0100

    pcf8574 working

--- goldboard/pcf8574.c
+++ goldboard/pcf8574.c
@@ -1,21 +1,26 @@
 #include "pcf8574.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include "sim_avr.h"
-#include "avr_twi.h"
-#include "i2c_sensor.h"
 
-void pcf8574_init(struct avr_t* avr, pcf8574_t* pcf, int adress){
-	i2c_sensor_init(avr, &pcf->i2c_sensor, adress, 1, 0, 1024);
-	i2c_sensor_attach(avr, &pcf->i2c_sensor, AVR_IOCTL_TWI_GETIRQ(0));
+static uint8_t i2c_read_callback(i2c_device_t* device){
+	pcf8574_t* p = (pcf8574_t*)(device->param);
+	return p->output_port;
+}
+
+static void i2c_write_callback(i2c_device_t* device, uint8_t data){
+	pcf8574_t* p = (pcf8574_t*)device->param;
+	p->input_port = data;
+}
+
+void pcf8574_init(struct avr_t* avr, pcf8574_t* pcf, uint8_t adress){
+	pcf->i2c_device.param = (void*)pcf;
+	i2c_device_init(avr, &pcf->i2c_device, adress, 1, &i2c_read_callback, &i2c_write_callback);
+	i2c_device_attach(avr, &pcf->i2c_device, AVR_IOCTL_TWI_GETIRQ(0));
 }
 
 
-int pcf8574_getValue(pcf8574_t* pcf){
-  return pcf->i2c_sensor.input[0];
+uint8_t pcf8574_getValue(pcf8574_t* pcf){
+  	return pcf->input_port;
 }
 
-void pcf8574_setValue(pcf8574_t* pcf, int data){
-  pcf->i2c_sensor.output[0] = data;
+void pcf8574_setValue(pcf8574_t* pcf, uint8_t data){
+  	pcf->output_port = data;
 }
